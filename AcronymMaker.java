e86c220d5c20061fc192360be6ba2f0e1af8de98
Josh Batchelor <josh_batch@live.com.au>
2013-12-31 00:16:49 +0800

    Setup start of project
    -Included old ConsoleIO class
    -Added a main class
    -added makefile

--- AcronymMaker.java
+++ AcronymMaker.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AcronymMaker
 {
 	public AcronymMaker()
 	{
-	
+		 
 	}
 }
